e43dbddec9329efc4bf29dd73981d664c3ff0f5e
Stefan Vivallius <varulvsnatt@hotmail.com>
2022-09-11 17:15:46 +0200

    Add dices in dicemanager class

---
+++ DiceManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    static class DiceRoller
+    {
+        static List<Dice> dices;
+    }
+    public class Dice
+    {
+        private int amount;
+        public Dice(int num)
+        {
+            this.amount = num;
+        }
+    }
+
+    public class D4 : Dice
+    {
+        private int sides = 4;
+
+        public D4(int amount) :base(amount)
+        {
+            
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace DiceRoller
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
