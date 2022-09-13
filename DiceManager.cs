7d030cef9d81e05d1658fa33e643d5ecd0c69059
Stefan Vivallius <varulvsnatt@hotmail.com>
2022-09-13 15:23:05 +0200

    Added dice selection to interface. No function attached.

--- DiceManager.cs
+++ DiceManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pbx_Add_D4 = new System.Windows.Forms.PictureBox();
+            this.pbx_Add_D6 = new System.Windows.Forms.PictureBox();
+            this.pbx_Add_D8 = new System.Windows.Forms.PictureBox();
+            this.pbx_Add_D10 = new System.Windows.Forms.PictureBox();
+            this.pbx_Add_D12 = new System.Windows.Forms.PictureBox();
+            this.pbx_Add_D20 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D20)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pbx_Add_D4
+            // 
+            this.pbx_Add_D4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D4.Image = global::DiceRoller.Properties.Resources.d41;
+            this.pbx_Add_D4.Location = new System.Drawing.Point(12, 12);
+            this.pbx_Add_D4.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D4.Name = "pbx_Add_D4";
+            this.pbx_Add_D4.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D4.TabIndex = 0;
+            this.pbx_Add_D4.TabStop = false;
+            // 
+            // pbx_Add_D6
+            // 
+            this.pbx_Add_D6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D6.Image = global::DiceRoller.Properties.Resources.d6;
+            this.pbx_Add_D6.Location = new System.Drawing.Point(12, 70);
+            this.pbx_Add_D6.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D6.Name = "pbx_Add_D6";
+            this.pbx_Add_D6.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D6.TabIndex = 1;
+            this.pbx_Add_D6.TabStop = false;
+            // 
+            // pbx_Add_D8
+            // 
+            this.pbx_Add_D8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D8.Image = global::DiceRoller.Properties.Resources.d8;
+            this.pbx_Add_D8.Location = new System.Drawing.Point(12, 128);
+            this.pbx_Add_D8.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D8.Name = "pbx_Add_D8";
+            this.pbx_Add_D8.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D8.TabIndex = 2;
+            this.pbx_Add_D8.TabStop = false;
+            // 
+            // pbx_Add_D10
+            // 
+            this.pbx_Add_D10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D10.Image = global::DiceRoller.Properties.Resources.d10;
+            this.pbx_Add_D10.Location = new System.Drawing.Point(12, 186);
+            this.pbx_Add_D10.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D10.Name = "pbx_Add_D10";
+            this.pbx_Add_D10.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D10.TabIndex = 3;
+            this.pbx_Add_D10.TabStop = false;
+            // 
+            // pbx_Add_D12
+            // 
+            this.pbx_Add_D12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D12.Image = global::DiceRoller.Properties.Resources.d12;
+            this.pbx_Add_D12.Location = new System.Drawing.Point(12, 244);
+            this.pbx_Add_D12.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D12.Name = "pbx_Add_D12";
+            this.pbx_Add_D12.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D12.TabIndex = 4;
+            this.pbx_Add_D12.TabStop = false;
+            // 
+            // pbx_Add_D20
+            // 
+            this.pbx_Add_D20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbx_Add_D20.Image = global::DiceRoller.Properties.Resources.d41;
+            this.pbx_Add_D20.Location = new System.Drawing.Point(12, 302);
+            this.pbx_Add_D20.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pbx_Add_D20.Name = "pbx_Add_D20";
+            this.pbx_Add_D20.Size = new System.Drawing.Size(52, 52);
+            this.pbx_Add_D20.TabIndex = 5;
+            this.pbx_Add_D20.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(611, 430);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ClientSize = new System.Drawing.Size(582, 373);
+            this.Controls.Add(this.pbx_Add_D20);
+            this.Controls.Add(this.pbx_Add_D12);
+            this.Controls.Add(this.pbx_Add_D10);
+            this.Controls.Add(this.pbx_Add_D8);
+            this.Controls.Add(this.pbx_Add_D6);
+            this.Controls.Add(this.pbx_Add_D4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 420);
+            this.MinimumSize = new System.Drawing.Size(600, 420);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Dice Roller - For your really big numbers";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D20)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private PictureBox pbx_Add_D4;
+        private PictureBox pbx_Add_D6;
+        private PictureBox pbx_Add_D8;
+        private PictureBox pbx_Add_D10;
+        private PictureBox pbx_Add_D12;
+        private PictureBox pbx_Add_D20;
     }
 }
