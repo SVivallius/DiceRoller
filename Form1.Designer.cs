8c9a201d2c168bc0a66d1211f68e3e3472b3b54e
Stefan Vivallius <varulvsnatt@hotmail.com>
2022-09-13 22:46:34 +0200

    Design finished. Add functionality to buttons.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,6 +34,15 @@
             this.pbx_Add_D10 = new System.Windows.Forms.PictureBox();
             this.pbx_Add_D12 = new System.Windows.Forms.PictureBox();
             this.pbx_Add_D20 = new System.Windows.Forms.PictureBox();
+            this.amount_D4 = new System.Windows.Forms.Label();
+            this.amount_D6 = new System.Windows.Forms.Label();
+            this.amount_D8 = new System.Windows.Forms.Label();
+            this.amount_D10 = new System.Windows.Forms.Label();
+            this.amount_D12 = new System.Windows.Forms.Label();
+            this.amount_D20 = new System.Windows.Forms.Label();
+            this.btn_Roll_Dice = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D6)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D8)).BeginInit();
@@ -100,7 +109,7 @@
             // pbx_Add_D20
             // 
             this.pbx_Add_D20.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pbx_Add_D20.Image = global::DiceRoller.Properties.Resources.d41;
+            this.pbx_Add_D20.Image = global::DiceRoller.Properties.Resources.d20;
             this.pbx_Add_D20.Location = new System.Drawing.Point(12, 302);
             this.pbx_Add_D20.MinimumSize = new System.Drawing.Size(50, 50);
             this.pbx_Add_D20.Name = "pbx_Add_D20";
@@ -108,11 +117,110 @@
             this.pbx_Add_D20.TabIndex = 5;
             this.pbx_Add_D20.TabStop = false;
             // 
+            // amount_D4
+            // 
+            this.amount_D4.AutoSize = true;
+            this.amount_D4.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D4.Location = new System.Drawing.Point(91, 12);
+            this.amount_D4.Name = "amount_D4";
+            this.amount_D4.Size = new System.Drawing.Size(46, 54);
+            this.amount_D4.TabIndex = 7;
+            this.amount_D4.Text = "0";
+            // 
+            // amount_D6
+            // 
+            this.amount_D6.AutoSize = true;
+            this.amount_D6.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D6.Location = new System.Drawing.Point(91, 70);
+            this.amount_D6.Name = "amount_D6";
+            this.amount_D6.Size = new System.Drawing.Size(46, 54);
+            this.amount_D6.TabIndex = 8;
+            this.amount_D6.Text = "0";
+            // 
+            // amount_D8
+            // 
+            this.amount_D8.AutoSize = true;
+            this.amount_D8.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D8.Location = new System.Drawing.Point(91, 126);
+            this.amount_D8.Name = "amount_D8";
+            this.amount_D8.Size = new System.Drawing.Size(46, 54);
+            this.amount_D8.TabIndex = 9;
+            this.amount_D8.Text = "0";
+            // 
+            // amount_D10
+            // 
+            this.amount_D10.AutoSize = true;
+            this.amount_D10.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D10.Location = new System.Drawing.Point(91, 184);
+            this.amount_D10.Name = "amount_D10";
+            this.amount_D10.Size = new System.Drawing.Size(46, 54);
+            this.amount_D10.TabIndex = 10;
+            this.amount_D10.Text = "0";
+            // 
+            // amount_D12
+            // 
+            this.amount_D12.AutoSize = true;
+            this.amount_D12.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D12.Location = new System.Drawing.Point(91, 242);
+            this.amount_D12.Name = "amount_D12";
+            this.amount_D12.Size = new System.Drawing.Size(46, 54);
+            this.amount_D12.TabIndex = 11;
+            this.amount_D12.Text = "0";
+            // 
+            // amount_D20
+            // 
+            this.amount_D20.AutoSize = true;
+            this.amount_D20.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.amount_D20.Location = new System.Drawing.Point(91, 300);
+            this.amount_D20.Name = "amount_D20";
+            this.amount_D20.Size = new System.Drawing.Size(46, 54);
+            this.amount_D20.TabIndex = 12;
+            this.amount_D20.Text = "0";
+            // 
+            // btn_Roll_Dice
+            // 
+            this.btn_Roll_Dice.Font = new System.Drawing.Font("Franklin Gothic Book", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btn_Roll_Dice.Location = new System.Drawing.Point(175, 279);
+            this.btn_Roll_Dice.Name = "btn_Roll_Dice";
+            this.btn_Roll_Dice.Size = new System.Drawing.Size(180, 75);
+            this.btn_Roll_Dice.TabIndex = 13;
+            this.btn_Roll_Dice.Text = "Roll!";
+            this.btn_Roll_Dice.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Book", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(378, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 75);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Clear dice";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(309, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 81);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(582, 373);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Roll_Dice);
+            this.Controls.Add(this.amount_D20);
+            this.Controls.Add(this.amount_D12);
+            this.Controls.Add(this.amount_D10);
+            this.Controls.Add(this.amount_D8);
+            this.Controls.Add(this.amount_D6);
+            this.Controls.Add(this.amount_D4);
             this.Controls.Add(this.pbx_Add_D20);
             this.Controls.Add(this.pbx_Add_D12);
             this.Controls.Add(this.pbx_Add_D10);
@@ -133,6 +241,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D12)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbx_Add_D20)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -144,5 +253,14 @@
         private PictureBox pbx_Add_D10;
         private PictureBox pbx_Add_D12;
         private PictureBox pbx_Add_D20;
+        public Label amount_D4;
+        public Label amount_D6;
+        public Label amount_D8;
+        public Label amount_D10;
+        public Label amount_D12;
+        public Label amount_D20;
+        private Button btn_Roll_Dice;
+        private Button button1;
+        public Label label1;
     }
 }
---
+++ How many.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DiceRoller
+{
+    partial class How_many
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OK = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.tbx_amount_Entered = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OK
+            // 
+            this.OK.Location = new System.Drawing.Point(93, 98);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(94, 29);
+            this.OK.TabIndex = 0;
+            this.OK.Text = "Ok!";
+            this.OK.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(193, 98);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(94, 29);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // tbx_amount_Entered
+            // 
+            this.tbx_amount_Entered.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbx_amount_Entered.Location = new System.Drawing.Point(12, 53);
+            this.tbx_amount_Entered.Name = "tbx_amount_Entered";
+            this.tbx_amount_Entered.Size = new System.Drawing.Size(275, 39);
+            this.tbx_amount_Entered.TabIndex = 2;
+            this.tbx_amount_Entered.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "How many to add\r\n(only supports numerics)";
+            // 
+            // How_many
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 139);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbx_amount_Entered);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.OK);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 200);
+            this.MinimizeBox = false;
+            this.Name = "How_many";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "How much will this hurt?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button OK;
+        private Button cancel;
+        private TextBox tbx_amount_Entered;
+        private Label label1;
+    }
+}
